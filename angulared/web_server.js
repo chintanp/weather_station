f0c3fcd27da690b29b2e749d844fd76f7f3beb88
Chintan Pathak <chintan.pathak@gmail.com>
2014-04-10 10:26:14 +0530

    Create web_server.js

---
+++ angulared/web_server.js
@@ -0,0 +1,389 @@
+#!/usr/bin/env node
+
+var util = require('util'),
+    http = require('http'),
+    fs = require('fs'),
+    net = require('net'),           // For TCP communication with datalogger
+    url = require('url'),
+    events = require('events'),
+    path = require('path');         //For manipulating file-paths
+
+//For Communication with the Datalogger
+var TCP_PORT = 7700;
+var TCP_HOST = '172.16.102.16';
+
+//Required for socket.io
+var parts1 = __dirname.split('\\');
+console.log(parts1);
+parts1.splice(parts1.length - 1);
+console.log(parts1);
+console.log(parts1.join('/'));
+//var index = fs.readFileSync(parts1.join('/') + '/app/index.html');
+
+//Connecting to MongoDB 
+
+//Name of the database - default to localhost
+var databaseUrl = "weatherdb"; // "username:password@example.com/mydb"
+
+//Name of the collections
+var collections = ["raw_data", "data_reports"]  //raw_data contains the data as obtained ....//data_reports contains daily/periodic reports generated on data.
+
+//Connect to the database - using the module mongojs - which exposes mongodb like API to node
+var db = require("mongojs").connect(databaseUrl, collections);
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Emit welcome message on connection
+io.sockets.on('connection', function(http_socket) {
+    http_socket.emit('welcome', { message: 'Welcome!' });
+    http_socket.on('i am client', console.log);
+});
+
+//Create a TCP socket to read data from datalogger
+var tcp_socket = net.createConnection(TCP_PORT, TCP_HOST);
+
+//Socket Object exposes many events through 'on' - namely 'connect', 'data', 'end', 'error'
+tcp_socket.on('error', function(error) {
+  console.log("Error Connecting to TCP host: " + TCP_HOST);
+});
+
+tcp_socket.on('connect', function(connect) {
+  
+  console.log('connection established with TCP host: ' + TCP_HOST);
+  tcp_socket.setEncoding('ascii');
+});
+
+tcp_socket.on('data', function(data) {
+        
+  console.log('DATA ' + tcp_socket.remoteAddress + ': ' + data);
+
+  
+
+  //var weatherdata = data.livedata;
+  
+  //Parse the output from weather station into local variables that contain individual sensor's values
+     
+  //Declare arrays
+  var dataArray = new Array();
+  var dbObject = [];
+
+  //Split to get the array
+  var dataArray = data.split("\r\n");
+
+
+  for(var i = 0; i < dataArray.length-1; i++) {
+
+    //console.log(dataArray[i]);
+    if(dataArray[i].split(" ")[0] != '') {
+      sensorName = dataArray[i].split(" ")[0];
+      sensorValue = dataArray[i].split(" ")[1] ;
+      sensorUnit = dataArray[i].split(" ")[2] ;
+      console.log(sensorName + " : " + sensorValue + " : " + sensorUnit );
+
+      //Adding data to the dbObject data
+      dbObject.push({"sensor" : sensorName, "value": sensorValue, "unit" : sensorUnit });
+    }  
+  }
+  
+  //Convert the object to string
+  JSON.stringify(dbObject);
+  console.log(typeof(dbObject));
+  
+  io.sockets.emit('livedata', { livedata: data });
+  //Insert into Mongodb database
+  db.raw_data.insert(dbObject, function(err, saved){
+    if( err || !saved ) console.log("Data not saved");
+    else console.log("Data saved");
+  });
+
+  //To read about the format of returned string: refer to DT80 manual Pg: 22
+  // var recordType = dataArray[0];
+  // var serialNumber = dataArray[1];
+  // var jobName = dataArray[2];
+  // var date = dataArray[3];
+  // var time = dataArray[4];
+  // var subsec = dataArray[5];
+  // var realtime = dataArray[6];
+  // //var schedule = dataArray[7];          //Schedule is preceded by a semi-colon
+  // var indexFirst = dataArray[7];
+  // var firstData = dataArray[8];           //Temperature
+  // var secondData = dataArray[9];         //Humidity
+  // var thirdData = dataArray[10];          //Atmospheric Pressure
+  // var fourthData = dataArray[11];         //Internal Temperature
+  // var fifthData = dataArray[12];          //Wind Direction
+  // var sixthData = dataArray[13];          //Wind Speed
+  // var seventhData = dataArray[14];        //Rain    
+  // var lastData = dataArray[15].split(";");     
+  // var eighthData = lastData[0];            //Solar Radiation    
+  // var ninthData = dataArray[17];         //CRC error check   
+
+  // //Insert the data in database - as a JSON object. 
+  // db.raw_data.insert({"date": date, 
+  //                     "time": time, 
+  //                     "subsec": subsec, 
+  //                     "Temperature" : firstData, 
+  //                     "Humidity" : secondData, 
+  //                     "Atmospheric Pressure" : thirdData,
+  //                     "Wind Direction" : fifthData,
+  //                     "Wind Speed" : sixthData,
+  //                     "Rain" : seventhData, 
+  //                     "Solar Radiation" : eighthData
+  //                     }, function(err, saved) {
+  // if( err || !saved ) console.log("Data not saved");
+  // else console.log("Data saved");
+  // });
+});
+
+tcp_socket.on('end', function() {
+  console.log('socket closing...');
+});
+  
+app.listen(3000);
+
+/* _______________________________________________________________________ */
+
+//Supplied with Angular
+
+var DEFAULT_PORT = 8000;
+
+function main(argv) {
+  new HttpServer({
+    'GET': createServlet(StaticServlet),
+    'HEAD': createServlet(StaticServlet)
+  }).start(Number(argv[2]) || DEFAULT_PORT);
+}
+
+function escapeHtml(value) {
+  return value.toString().
+    replace('<', '&lt;').
+    replace('>', '&gt;').
+    replace('"', '&quot;');
+}
+
+function createServlet(Class) {
+  var servlet = new Class();
+  return servlet.handleRequest.bind(servlet);
+}
+
+/**
+ * An Http server implementation that uses a map of methods to decide
+ * action routing.
+ *
+ * @param {Object} Map of method => Handler function
+ */
+function HttpServer(handlers) {
+  this.handlers = handlers;
+  this.server = http.createServer(this.handleRequest_.bind(this));
+}
+
+HttpServer.prototype.start = function(port) {
+  this.port = port;
+  this.server.listen(port);
+  util.puts('Http Server running at http://localhost:' + port + '/');
+};
+
+HttpServer.prototype.parseUrl_ = function(urlString) {
+  var parsed = url.parse(urlString);
+  parsed.pathname = url.resolve('/', parsed.pathname);
+  return url.parse(url.format(parsed), true);
+};
+
+HttpServer.prototype.handleRequest_ = function(req, res) {
+  var logEntry = req.method + ' ' + req.url;
+  if (req.headers['user-agent']) {
+    logEntry += ' ' + req.headers['user-agent'];
+  }
+  util.puts(logEntry);
+  req.url = this.parseUrl_(req.url);
+  var handler = this.handlers[req.method];
+  if (!handler) {
+    res.writeHead(501);
+    res.end();
+  } else {
+    handler.call(this, req, res);
+  }
+};
+
+/**
+ * Handles static content.
+ */
+function StaticServlet() {}
+
+StaticServlet.MimeMap = {
+  'txt': 'text/plain',
+  'html': 'text/html',
+  'css': 'text/css',
+  'xml': 'application/xml',
+  'json': 'application/json',
+  'js': 'application/javascript',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'gif': 'image/gif',
+  'png': 'image/png',
+  'svg': 'image/svg+xml'
+};
+
+StaticServlet.prototype.handleRequest = function(req, res) {
+  var self = this;
+  var path = ('./' + req.url.pathname).replace('//','/').replace(/%(..)/g, function(match, hex){
+    return String.fromCharCode(parseInt(hex, 16));
+  });
+  var parts = path.split('/');
+  if (parts[parts.length-1].charAt(0) === '.')
+    return self.sendForbidden_(req, res, path);
+  fs.stat(path, function(err, stat) {
+    if (err)
+      return self.sendMissing_(req, res, path);
+    if (stat.isDirectory())
+      return self.sendDirectory_(req, res, path);
+    return self.sendFile_(req, res, path);
+  });
+}
+
+StaticServlet.prototype.sendError_ = function(req, res, error) {
+  res.writeHead(500, {
+      'Content-Type': 'text/html'
+  });
+  res.write('<!doctype html>\n');
+  res.write('<title>Internal Server Error</title>\n');
+  res.write('<h1>Internal Server Error</h1>');
+  res.write('<pre>' + escapeHtml(util.inspect(error)) + '</pre>');
+  util.puts('500 Internal Server Error');
+  util.puts(util.inspect(error));
+};
+
+StaticServlet.prototype.sendMissing_ = function(req, res, path) {
+  path = path.substring(1);
+  res.writeHead(404, {
+      'Content-Type': 'text/html'
+  });
+  res.write('<!doctype html>\n');
+  res.write('<title>404 Not Found</title>\n');
+  res.write('<h1>Not Found</h1>');
+  res.write(
+    '<p>The requested URL ' +
+    escapeHtml(path) +
+    ' was not found on this server.</p>'
+  );
+  res.end();
+  util.puts('404 Not Found: ' + path);
+};
+
+StaticServlet.prototype.sendForbidden_ = function(req, res, path) {
+  path = path.substring(1);
+  res.writeHead(403, {
+      'Content-Type': 'text/html'
+  });
+  res.write('<!doctype html>\n');
+  res.write('<title>403 Forbidden</title>\n');
+  res.write('<h1>Forbidden</h1>');
+  res.write(
+    '<p>You do not have permission to access ' +
+    escapeHtml(path) + ' on this server.</p>'
+  );
+  res.end();
+  util.puts('403 Forbidden: ' + path);
+};
+
+StaticServlet.prototype.sendRedirect_ = function(req, res, redirectUrl) {
+  res.writeHead(301, {
+      'Content-Type': 'text/html',
+      'Location': redirectUrl
+  });
+  res.write('<!doctype html>\n');
+  res.write('<title>301 Moved Permanently</title>\n');
+  res.write('<h1>Moved Permanently</h1>');
+  res.write(
+    '<p>The document has moved <a href="' +
+    redirectUrl +
+    '">here</a>.</p>'
+  );
+  res.end();
+  util.puts('301 Moved Permanently: ' + redirectUrl);
+};
+
+StaticServlet.prototype.sendFile_ = function(req, res, path) {
+  var self = this;
+  var file = fs.createReadStream(path);
+  res.writeHead(200, {
+    'Content-Type': StaticServlet.
+      MimeMap[path.split('.').pop()] || 'text/plain'
+  });
+  if (req.method === 'HEAD') {
+    res.end();
+  } else {
+    file.on('data', res.write.bind(res));
+    file.on('close', function() {
+      res.end();
+    });
+    file.on('error', function(error) {
+      self.sendError_(req, res, error);
+    });
+  }
+};
+
+StaticServlet.prototype.sendDirectory_ = function(req, res, path) {
+  var self = this;
+  if (path.match(/[^\/]$/)) {
+    req.url.pathname += '/';
+    var redirectUrl = url.format(url.parse(url.format(req.url)));
+    return self.sendRedirect_(req, res, redirectUrl);
+  }
+  fs.readdir(path, function(err, files) {
+    if (err)
+      return self.sendError_(req, res, error);
+
+    if (!files.length)
+      return self.writeDirectoryIndex_(req, res, path, []);
+
+    var remaining = files.length;
+    files.forEach(function(fileName, index) {
+      fs.stat(path + '/' + fileName, function(err, stat) {
+        if (err)
+          return self.sendError_(req, res, err);
+        if (stat.isDirectory()) {
+          files[index] = fileName + '/';
+        }
+        if (!(--remaining))
+          return self.writeDirectoryIndex_(req, res, path, files);
+      });
+    });
+  });
+};
+
+StaticServlet.prototype.writeDirectoryIndex_ = function(req, res, path, files) {
+  path = path.substring(1);
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  if (req.method === 'HEAD') {
+    res.end();
+    return;
+  }
+  res.write('<!doctype html>\n');
+  res.write('<title>' + escapeHtml(path) + '</title>\n');
+  res.write('<style>\n');
+  res.write('  ol { list-style-type: none; font-size: 1.2em; }\n');
+  res.write('</style>\n');
+  res.write('<h1>Directory: ' + escapeHtml(path) + '</h1>');
+  res.write('<ol>');
+  files.forEach(function(fileName) {
+    if (fileName.charAt(0) !== '.') {
+      res.write('<li><a href="' +
+        escapeHtml(fileName) + '">' +
+        escapeHtml(fileName) + '</a></li>');
+    }
+  });
+  res.write('</ol>');
+  res.end();
+};
+
+// Must be last,
+main(process.argv);
